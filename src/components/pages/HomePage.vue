8ecf59b5128786b8dd8e946807265b8c1120e9b0
HikmetDonmez <61684718+HikmetDonmez@users.noreply.github.com>
2021-03-25 01:06:33 +0300

    User page and validation added
    User page and validation added

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container mt-5 mb-5" style="width: 500px">
       <FormValidation />
+      <div>
+          <li v-for="user in loginUsers" v-bind:key="user.name">
+              <span class="name">{{user.name}}</span>
+              <span class="email">{{user.email}}</span>
+          </li>
+      </div>
   </div>
 </template>
 
@@ -10,6 +16,11 @@ import FormValidation from '../validation/FormValidation'
 export default {
     components: {
         FormValidation
+    },
+    computed: {
+        loginUsers(){
+            return this.$store.state.loginUsers;
+        }
     }
 }
 
